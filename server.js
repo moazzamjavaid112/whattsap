b3acadc75e0cbffb3ec40d5c16cdc7791be36f67
Moazzam Javaid <146067115+moazzamjavaid112@users.noreply.github.com>
2025-09-16 12:40:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -301,7 +301,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/check", (req, res) => {
+  res.send("Hello World");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Server listening on http://localhost:${PORT}`)
 );
+
